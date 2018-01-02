32542e2e0392b10997ac0e55c46ee8a3ac4f8b4e
bender <bender@unit22.org>
2018-01-02 16:21:22 +0000

    python3 compatible sockets

--- fetchd/server.py
+++ fetchd/server.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python
 
-from SocketServer import TCPServer, StreamRequestHandler
 import socket
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+HOST = ''
+PORT = 50007
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-class DownloadManger(StreamRequestHandler):
+def run():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
 
-    def handle(self):
-        self.data = self.rfile.readline().strip()
-        logging.info("From <%s>: %s" % (self.client_address, self.data))
-        self.wfile.write(self.data.upper() + "\r\n")
-
-
-class Server(TCPServer):
-
-    # The constant would be better initialized by a systemd module
-    SYSTEMD_FIRST_SOCKET_FD = 3
-    HOST = 'localhost'
-    PORT = 9999
-
-    def __init__(self, handler_cls):
-        # Invoke base but omit bind/listen steps (performed by systemd activation!)
-        server_address = (self.HOST, self.PORT)
-        super().__init__(self, server_address, handler_cls,
-                         bind_and_activate=False)
-
-        # Override socket
-        self.socket = socket.fromfd(self.SYSTEMD_FIRST_SOCKET_FD,
-                                    self.address_family, self.socket_type)
+        try:
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    logging.info('Connection from %s', addr)
+                    while True:
+                        data = conn.recv(1024)
+                        if not data: break
+                        conn.sendall(data)
+        finally:
+            connection.close()
 
 
 if __name__ == "__main__":
-    handler = DownloadManger()
-    server = Server(handler)
-    server.serve_forever()
+    run()
